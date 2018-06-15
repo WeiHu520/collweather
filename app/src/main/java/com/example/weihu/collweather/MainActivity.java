718fd77117b9682a481996199e231938e0f8e48b
wei.hu <hw251921193@163.com>
2018-06-15 16:33:25 +0800

    aaaa.

---
+++ app/src/main/java/com/example/weihu/collweather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.weihu.collweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //
+        setContentView(R.layout.activity_main);
+    }
+}
