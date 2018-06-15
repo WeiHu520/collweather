b01da99a0715e8e4ecc76b4225556860c5f77710
wei.hu <hw251921193@163.com>
2018-06-15 16:38:36 +0800

    你们好吗.

--- app/src/main/java/com/example/weihu/collweather/MainActivity.java
+++ app/src/main/java/com/example/weihu/collweather/MainActivity.java
@@ -9,7 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //
+        //nihaoaoaoof
+
+        
         setContentView(R.layout.activity_main);
     }
 }
